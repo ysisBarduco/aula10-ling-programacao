5f58d0f55f8af9a752d13399e606f344f803defa
Ysis Barduco <ysisbarduco@gmail.com>
2025-10-30 18:58:50 -0300

    Correções na leitura de valores, exibição de números pares implementada.

--- exercicio1.c
+++ exercicio1.c
@@ -8,13 +8,44 @@ Exercício 1: Vetor de Números Pares
   - Libere a memória
 */
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    int n, i;
-    int *vetor;
+  int quantidade;
+  int i;
+  int *vetor;
 
-    //Solicita ao usuário quantos números ele quer digitar
-    printf("\nQuantos valores deseja salvar no vetor?");
-    scanf("%d", &n);
-    vetor = (int*) malloc(n * sizeof(int));
+  //Solicita ao usário quantos números ele quer digitar
+  printf("\nQuantos numeros deseja armazenar no vetor? ");
+  scanf("%d", &quantidade);
+
+  //Alocar dinamicamente um vetor
+  vetor = (int*) malloc(quantidade * sizeof(int));
+
+  //Verifica se a alocação foi bem sucedida
+  if(vetor == NULL) {
+    printf("\nErro ao alocar memória!\n");
+    return 1;
+  }
+  
+  //Lê os números
+  printf("\nDigite: \n");
+  for (i = 0; i < quantidade; i++) {
+    printf(" o %d numero: ", i + 1);
+    scanf("%d", &vetor[i]);
+  }
+
+  //Exibe os números pares
+  printf("\nNumeros pares digitados: ");
+  for(i = 0; i < quantidade; i++) {
+    if((vetor[i] % 2) == 0) {
+      printf("%d ", vetor[i]);
+    }
+  }
+
+  //Libera a memória
+  vetor = (int*) realloc(vetor, 0 * sizeof(int));
+  free(vetor);
+
+  return 0;
 }
